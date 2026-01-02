22aa526023a9ce0dbc422161c1544072e790aa8e
HP <harishpranavtech@gmail.com>
2026-01-02 13:55:57 +0530

    "updated client.ts"

--- src/lib/appwrite/client.ts
+++ src/lib/appwrite/client.ts
@@ -1,11 +1,12 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
-const client = new Client()
+const client = new Client();
+client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
-export const account = new Account(client);
-export const databases = new Databases(client);
-export const storage = new Storage(client);
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
 
-export { client, Account, Databases, Storage };
+export { client, account, databases, storage };
